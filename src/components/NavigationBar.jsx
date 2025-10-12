f6ec38ee4a0fc3e6b8386ee5b1925a76165290e1
bhumipandey06 <bhumipandey1188@gmail.com>
2025-10-12 11:30:19 +0530

    updated register button

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -84,14 +84,6 @@ const NavigationBar = () => {
             </button>
           ))}
 
-          <a
-            href="https://forms.gle/cPSbdNEQMMkcoNX3A"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block w-full text-center px-4 py-2 bg-gradient-to-r from-green-500 to-blue-500 rounded-full text-white font-semibold hover:scale-105 transition-transform"
-          >
-            Register
-          </a>
         </div>
       )}
     </nav>
