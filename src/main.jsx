37daaa3e43823379842001ef201501e4ec51eccb
Ayush saini <ayushsaini7717@gmail.com>
2025-11-14 17:45:10 +0530

    test unauthorized docker access

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path='/' element={<App/>}></Route>
       <Route path='/info' element={<NoticesPage/>}></Route>
+      
     </Routes>
     </BrowserRouter>
   </StrictMode>,
